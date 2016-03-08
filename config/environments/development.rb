86731f0217497f42b3b1bd974f49b4d17d5bfff4
Andrew Cho <choap@usc.edu>
2016-03-08 10:46:06 -0800

    created program view page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,7 @@ Rails.application.configure do
 
   #devise requirement
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  #paperclip
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 end
